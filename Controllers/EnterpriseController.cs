2575e96db50504ad8f167b89ef2b4f37c770f63c
qsandrew <sanches_andrew@mail.ru>
2020-05-19 16:49:41 +0600

    пп

---
+++ Controllers/EnterpriseController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Controllers
+{
+    public class EnterpriseController : Controller
+    {
+        public IActionResult Employee()
+        {
+            return View();
+        }
+    }
+}
